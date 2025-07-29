48e17a61f6623db490384ea6bc828f3019ef30e4
nandambindu678 <149202578+nandambindu678@users.noreply.github.com>
2025-07-29 10:24:30 +0530

    Experiment-1

--- mlp_cifar.py
+++ mlp_cifar.py
@@ -14,9 +14,6 @@ y_test = to_categorical(y_test)
 #Build the architecture
 model = Sequential()
 model.add(Flatten(input_shape=(32,32,3)))
-model.add(Dense(1024,activation='relu'))
-model.add(Dense(512,activation='relu'))
-model.add(Dense(256,activation='relu'))
 model.add(Dense(128,activation='relu'))
 model.add(Dense(10,activation='softmax'))
 
@@ -24,7 +21,7 @@ model.add(Dense(10,activation='softmax'))
 model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
 
 #Train
-history=model.fit(X_train,y_train,epochs=30,batch_size=64,validation_split=0.2)
+history=model.fit(X_train,y_train,epochs=10,batch_size=64,validation_split=0.2)
 
 #Evaluate
 test_accuracy,loss = model.evaluate(X_test,y_test)
